3880151f8e7b91954775a414e30998295cf7c057
mohamedsayedms4 <mohamedms4002@gmail.com>
2026-02-14 03:49:38 +0200

    (conf) :  Initialize Spring Boot project structure &  Configure database connection (SQL Server/MySQL)

---
+++ src/main/java/org/example/academicmanagementsystem/AcademicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.academicmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicManagementSystemApplication.class, args);
+    }
+
+}
